b830b355cd98d18b7e171c366692308dac0ad5e4
Trevor Scheer <trevor.scheer@gmail.com>
2021-12-31 18:10:33 -0800

    add node-fetch

--- scripts/pr-comment.mjs
+++ scripts/pr-comment.mjs
@@ -1,9 +1,11 @@
+import fetch from "node-fetch";
+
 console.log(process.env.STORED_ARTIFACTS);
 console.log(process.env.CIRCLE_TOKEN);
 console.log(process.env.TESTING);
 console.log(process.env);
 
-(async() => {
+(async () => {
   const response = await fetch({
     url: "https://circleci.com/api/v2/project/github/trevor-scheer/apollo-utils/$CIRCLE_BUILD_NUM/artifacts",
     headers: {
@@ -13,4 +15,4 @@ console.log(process.env);
   });
 
   console.log(await response.json());
-})()
+})();
