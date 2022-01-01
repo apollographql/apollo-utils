f6f93d297e5b7a615cea2ef98258f99c0041fa92
Trevor Scheer <trevor.scheer@gmail.com>
2021-12-31 18:17:34 -0800

    test

--- scripts/pr-comment.mjs
+++ scripts/pr-comment.mjs
@@ -1,3 +1,4 @@
+// @ts-check
 import fetch from "node-fetch";
 
 console.log(process.env.CIRCLE_BUILD_NUM);
@@ -7,13 +8,15 @@ console.log(
 );
 
 (async () => {
-  const response = await fetch({
-    url: `https://circleci.com/api/v2/project/github/trevor-scheer/apollo-utils/${process.env.CIRCLE_BUILD_NUM}/artifacts`,
-    headers: {
-      Accept: "application/json",
-      Authorization: `Basic ${process.env.CIRCLE_TOKEN}`,
+  const response = await fetch(
+    `https://circleci.com/api/v2/project/github/trevor-scheer/apollo-utils/${process.env.CIRCLE_BUILD_NUM}/artifacts`,
+    {
+      headers: {
+        Accept: "application/json",
+        Authorization: `Basic ${process.env.CIRCLE_TOKEN}`,
+      },
     },
-  });
+  );
 
   console.log(await response.json());
 })();
