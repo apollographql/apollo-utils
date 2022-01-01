c4c1e64a2516d12df4d4deeb2322eee3343303e1
Trevor Scheer <trevor.scheer@gmail.com>
2021-12-31 18:08:05 -0800

    test

--- scripts/pr-comment.mjs
+++ scripts/pr-comment.mjs
@@ -2,3 +2,15 @@ console.log(process.env.STORED_ARTIFACTS);
 console.log(process.env.CIRCLE_TOKEN);
 console.log(process.env.TESTING);
 console.log(process.env);
+
+(async() => {
+  const response = await fetch({
+    url: "https://circleci.com/api/v2/project/github/trevor-scheer/apollo-utils/$CIRCLE_BUILD_NUM/artifacts",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Basic ${process.env.CIRCLE_TOKEN}`,
+    },
+  });
+
+  console.log(await response.json());
+})()
