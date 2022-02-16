c72ddde35edaeda9e7ebc31823e04c3343dc1302
Trevor Scheer <trevor.scheer@gmail.com>
2022-02-15 17:46:08 -0800

    Simplify hashing package to just createHash (#45)

--- packages/hashing/src/__tests__/operationHash.test.ts
+++
@@ -1,9 +0,0 @@
-import { operationHash } from "..";
-
-describe("operationHash", () => {
-  it("hashes an operation string", () => {
-    expect(operationHash("query { foo }")).toEqual(
-      "6aa12cae7e116b726dae8e1ea59ab89ea8b16b7d596c834341d10ca36e12efd6",
-    );
-  });
-});
--- packages/hashing/src/operationHash.ts
+++
@@ -1,5 +0,0 @@
-import { createHash } from ".";
-
-export function operationHash(operation: string): string {
-  return createHash("sha256").update(operation).digest("hex");
-}
