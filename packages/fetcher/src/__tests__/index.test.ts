8cc7755c634fa64692481db8fc0e41b446efc0c2
David Glasser <glasser@davidglasser.net>
2022-04-29 13:08:02 -0700

    fetcher: add test that undici is a Fetcher (#83)
    No code change.

--- packages/fetcher/src/__tests__/index.test.ts
+++ packages/fetcher/src/__tests__/index.test.ts
@@ -1,6 +1,7 @@
 import type { Fetcher } from "..";
 import nodeFetch from "node-fetch";
 import makeFetchHappen from "make-fetch-happen";
+import { fetch as undiciFetch } from "undici";
 
 // This "test suite" actually does all its work at compile time.
 function isAFetcher(_fetcher: Fetcher) {}
@@ -11,4 +12,8 @@ it("node-fetch is a Fetcher", () => {
 
 it("make-fetch-happen is a Fetcher", () => {
   isAFetcher(makeFetchHappen);
+});
+
+it("undici is a Fetcher", () => {
+  isAFetcher(undiciFetch);
 });
