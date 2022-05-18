4ace41838e807f05439a68bfec1ce8bae1c8bd9a
Trevor Scheer <trevor.scheer@gmail.com>
2022-05-18 14:15:10 -0700

    Add some commentary on removeAliases

--- packages/removeAliases/src/index.ts
+++ packages/removeAliases/src/index.ts
@@ -1,8 +1,10 @@
 // removeAliases gets rid of GraphQL aliases, a feature by which you can tell a
 // server to return a field's data under a different name from the field name.
 // Maybe this is useful if somebody somewhere inserts random aliases into their
-// queries.
-
+// queries. Note that this function makes no guarantees about the output and its
+// validity as a GraphQL operation, for example:
+// { x(a: 1) alias: x(a:2) } (valid) will yield
+// { x(a:1) x(a:2) } (invalid)
 import { DocumentNode, FieldNode, visit } from "graphql";
 
 export function removeAliases(ast: DocumentNode): DocumentNode {
