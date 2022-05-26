42330214b867b6585b5402eee9ec5b36f665ec32
Trevor Scheer <trevor.scheer@gmail.com>
2022-05-26 13:44:27 -0700

    Move sizeCalculation to static methon on InMemoryLRUCache

--- packages/keyValueCache/src/InMemoryLRUCache.ts
+++ packages/keyValueCache/src/InMemoryLRUCache.ts
@@ -7,12 +7,27 @@ export class InMemoryLRUCache<T = string> implements KeyValueCache<T> {
 
   constructor(lruCacheOpts?: LRUCache.Options<string, T>) {
     this.cache = new LRUCache({
-      sizeCalculation,
+      sizeCalculation: InMemoryLRUCache.sizeCalculation,
       // Create ~about~ a 30MiB cache by default. Configurable by providing
       // `lruCacheOpts`.
       maxSize: Math.pow(2, 20) * 30,
       ...lruCacheOpts,
     });
+  }
+
+  /**
+   * default size calculator for strings and serializable objects, else naively
+   * return 1
+   */
+  static sizeCalculation<T>(item: T) {
+    if (typeof item === "string") {
+      return item.length;
+    }
+    if (typeof item === "object") {
+      // will throw if the object has circular references
+      return Buffer.byteLength(JSON.stringify(item), "utf8");
+    }
+    return 1;
   }
 
   async set(key: string, value: T, options?: KeyValueCacheSetOptions) {
@@ -39,17 +54,4 @@ export class InMemoryLRUCache<T = string> implements KeyValueCache<T> {
     // LRUCache.keys() returns a generator (we just want an array)
     return [...this.cache.keys()];
   }
-}
-
-// default size calculator for strings and serializable objects, else naively
-// return 1
-export function sizeCalculation<T>(item: T) {
-  if (typeof item === "string") {
-    return item.length;
-  }
-  if (typeof item === "object") {
-    // will throw if the object has circular references
-    return Buffer.byteLength(JSON.stringify(item), "utf8");
-  }
-  return 1;
 }
