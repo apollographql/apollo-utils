44f60f3fa0db43afc6fe1ab4be50a9c295a5786e
Trevor Scheer <trevor.scheer@gmail.com>
2022-11-22 15:44:11 -0800

    iterate on tests

--- packages/fetcher/src/__tests__/index.test.ts
+++ packages/fetcher/src/__tests__/index.test.ts
@@ -1,14 +1,20 @@
-import type { Fetcher } from "..";
-import nodeFetch from "node-fetch";
-import makeFetchHappen from "make-fetch-happen";
-import { fetch as undiciFetch } from "undici";
 import FormData from "form-data";
+import makeFetchHappen from "make-fetch-happen";
 import nock from "nock";
+import nodeFetch from "node-fetch";
+import {
+  fetch as undiciFetch,
+  FormData as UndiciFormData,
+  MockAgent,
+  setGlobalDispatcher,
+} from "undici";
+import type { Fetcher } from "..";
+
 
 // This "test suite" actually does all its work at compile time.
-function isAFetcher(_fetcher: Fetcher) {}
-
 describe("implements Fetcher", () => {
+  function isAFetcher(_fetcher: Fetcher) {}
+
   it("node-fetch", () => {
     isAFetcher(nodeFetch);
   });
@@ -22,34 +28,78 @@ describe("implements Fetcher", () => {
   });
 });
 
-describe("accepts FormData as a body", () => {
+describe("Fetcher accepts FormData as a body", () => {
   beforeEach(nockBeforeEach);
   afterEach(nockAfterEach);
 
-  const formData = new FormData();
-  formData.append("foo", "bar");
-
   it("node-fetch", async () => {
-    nock("https://example.com").post("/", (body) => {
-      console.log("hello!");
-      console.log(body);
-      expect(body).toMatchInlineSnapshot();
-      return true;
-    });
-    const result = await nodeFetch("https://example.com/", {
+    const formData = new FormData();
+    formData.append("foo", "bar");
+
+    nock("https://example.com")
+      .post("/", (body) => {
+        expect(body).toMatch(`Content-Disposition: form-data; name="foo"\r\n\r\nbar`);
+        return true;
+      })
+      .reply(200, "ok");
+
+    await nodeFetch("https://example.com/", {
       method: "POST",
       body: formData,
-      headers: { "content-type": "x-www-form-urlencoded" },
     });
   });
 
-  // it("make-fetch-happen", () => {
-  //   makeFetchHappen('https://example.com', { body: formData });
-  // });
+  it("make-fetch-happen", async () => {
+    const formData = new FormData();
+    formData.append("foo", "bar");
 
-  // it("undici", () => {
-  //   undiciFetch('https://example.com', { body: formData });
-  // });
+    nock("https://example.com")
+      .post("/", (body) => {
+        expect(body).toMatch(`Content-Disposition: form-data; name="foo"\r\n\r\nbar`);
+        return true;
+      })
+      .reply(200, "ok");
+
+    await makeFetchHappen("https://example.com/", {
+      method: "POST",
+      body: formData,
+    });
+  });
+
+  it("undici", async () => {
+    // We can't use nock with undici, but undici provides mocking utilities
+    // https://github.com/nock/nock/issues/2183
+    const agent = new MockAgent({ connections: 1 });
+    agent.disableNetConnect();
+    setGlobalDispatcher(agent);
+
+    const client = agent.get("https://example.com");
+
+    client
+      .intercept({
+        path: "/",
+        method: "POST",
+        // This is unexpected. the body is typed as a string but it's the actual
+        // FormData object still. Pretty sure this is a bug.
+        body(body: any) {
+          const bar = body.get("foo");
+          return bar === "bar";
+        }
+      })
+      .reply(200, "ok");
+
+    // undici doesn't support `form-data`'s `FormData` class
+    const formData = new UndiciFormData();
+    formData.append("foo", "bar");
+
+    const result = await undiciFetch("https://example.com/", {
+      method: "POST",
+      body: formData,
+    });
+
+    const t = await result.text();
+    expect(t).toMatchInlineSnapshot(`"ok"`);
+  });
 });
 
 // Ensures an active and clean nock before every test
