783def7dd0004dce61288c9499c5d6d0acdd3609
Jerel Miller <jerelmiller@gmail.com>
2023-06-01 16:20:49 -0600

    Use cosmiconfig to load the config file

--- packages/generate-persisted-query-manifest/cli.js
+++ packages/generate-persisted-query-manifest/cli.js
@@ -1,8 +1,27 @@
 #!/usr/bin/env node
 
+const { cosmiconfig } = require("cosmiconfig");
 const { generatePersistedQueryManifest } = require("./dist/index.js");
+const { TypeScriptLoader } = require("cosmiconfig-typescript-loader");
 
-generatePersistedQueryManifest()
+const moduleName = "persisted-query-manifest";
+const supportedExtensions = ["json", "yml", "yaml", "js", "ts", "cjs"];
+
+const searchPlaces = supportedExtensions.flatMap((extension) => [
+  `.${moduleName}.config.${extension}`,
+  `${moduleName}.config.${extension}`,
+]);
+
+const explorer = cosmiconfig(moduleName, {
+  searchPlaces: searchPlaces.concat("package.json"),
+  loaders: {
+    ".ts": TypeScriptLoader(),
+  },
+});
+
+explorer
+  .search()
+  .then(() => generatePersistedQueryManifest())
   .then((manifest) => console.log(JSON.stringify(manifest, null, 2)))
   .catch((e) => {
     console.error(e);
