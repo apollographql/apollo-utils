ea6973726b0361b361d52c1b4e3ce3f89de18411
David Glasser <glasser@apollographql.com>
2025-01-02 10:16:55 -0800

    createhash: only call module.require if it exists (#466)
    Apparently Next.JS "Turbopack" runs code in a weird semi-Node
    environment that doesn't have module.require. We don't want to call
    require directly because that makes other bundlers put in polyfills. So
    make Turbopack use sha.js instead of just throwing. Non-ideal
    but better than what we have today.
    
    For apollographql/apollo-server#8004
    
    Co-authored-by: Trevor Scheer <trevor.scheer@gmail.com>

--- packages/createHash/src/index.ts
+++ packages/createHash/src/index.ts
@@ -1,7 +1,13 @@
 import { isNodeLike } from "@apollo/utils.isnodelike";
 
 export function createHash(kind: string): import("crypto").Hash {
-  if (isNodeLike) {
+  // Some Node-like environments (like next.js Turbopack) apparently
+  // don't have module.require, so double-check before we call it.
+  // (But don't change the value of isNodeLike because other logic depends on it,
+  // like Apollo Server signal handling defaults.) This does mean that
+  // Turbopack will call sha.js instead of the native crypto module, but
+  // it sure beats throwing because module.require does not exist.
+  if (isNodeLike && module.require) {
     // Use module.require instead of just require to avoid bundling whatever
     // crypto polyfills a non-Node bundler might fall back to.
     return module.require("crypto").createHash(kind);
