0c024424f786b31bb55195a765da992ec095adbb
David Glasser <glasser@apollographql.com>
2025-01-02 10:00:42 -0800

    Update packages/createHash/src/index.ts
    Co-authored-by: Trevor Scheer <trevor.scheer@gmail.com>

--- packages/createHash/src/index.ts
+++ packages/createHash/src/index.ts
@@ -3,7 +3,7 @@ import { isNodeLike } from "@apollo/utils.isnodelike";
 export function createHash(kind: string): import("crypto").Hash {
   // Some Node-like environments (like next.js Turbopack) apparently
   // don't have module.require, so double-check before we call it.
-  // (But don't the value of isNodeLike because other logic depends on it,
+  // (But don't change the value of isNodeLike because other logic depends on it,
   // like Apollo Server signal handling defaults.) This does mean that
   // Turbopack will call sha.js instead of the native crypto module, but
   // it sure beats throwing because module.require does not exist.
